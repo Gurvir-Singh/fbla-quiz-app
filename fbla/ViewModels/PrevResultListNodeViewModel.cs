5883c98b90f59f98bd880c45aa26fd8bbf29ea5e
Gurvir Singh <charizardzx9@gmail.com>
2021-02-22 06:24:08 -0500

    starting basis for prev results screen

---
+++ fbla/ViewModels/PrevResultListNodeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fbla.ViewModels
+{
+    public class PrevResultListNodeViewModel : ViewModelBase
+    {
+        public PrevResultListNodeViewModel(string Path)
+        {
+            DocName
+        }
+    }
+}
---
+++ fbla/Views/PrevResultListNodeView.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace fbla.Views
+{
+    public class PrevResultListNodeView : UserControl
+    {
+        public PrevResultListNodeView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
