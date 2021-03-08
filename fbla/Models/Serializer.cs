46f6341981632fc6e695aa6219e5ec669e8a133f
Gurvir Singh <charizardzx9@gmail.com>
2021-03-08 16:25:36 -0500

    trying out pdf images

--- fbla/Models/Serializer.cs
+++ fbla/Models/Serializer.cs
@@ -90,17 +90,26 @@ namespace fbla.Models
             pathPrefixString = pathPrefixString.Substring(0, pathPrefixString.Length - 2);
             String path = documentsPath + "\\FBLA Quiz Results\\";
             Document document = new Document();
-            Page page = new Page(PageSize.Letter, PageOrientation.Portrait, 54.0f);
-            document.Pages.Add(page);
+            //Page ScorePage = new Page(PageSize.Letter, PageOrientation.Portrait, 54.0f);
+            Page QustionsPage = new Page(PageSize.Letter, PageOrientation.Portrait, 54.0f);
+            //document.Pages.Add(ScorePage);
+            document.Pages.Add(QustionsPage);
+
+            //Page b = new Page()
+            //string docPath = (Directory.GetParent((Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))).FullName)).FullName;
+            //docPath += @"\source\repos\fbla-quiz-app\fbla\Assets\fblalogo.png";
+
+            //ScorePage.Elements.Add(new Image(docPath, 0, 0));
+
             int i = 0;
-            float questionHeight = (page.Dimensions.Height - 100) / 50;
-            float answerHeight = (page.Dimensions.Height - 100) / 5 * 0.9f;
+            float questionHeight = (QustionsPage.Dimensions.Height - 100) / 50;
+            float answerHeight = (QustionsPage.Dimensions.Height - 100) / 5 * 0.9f;
             foreach (dynamic d in questionsList)
             {
                 if (d.GetType() == (new MultipleChoiceQuestionViewModel()).GetType())
                 {
                     string lineText = "";
-                    page.Elements.Add(new Label("Quistion: " + d.questionModel.Question, 0, ((page.Dimensions.Height - 100) / 5 * i), page.Dimensions.Width - 100, questionHeight, Font.Helvetica, 14, TextAlign.Left));
+                    QustionsPage.Elements.Add(new Label("Quistion: " + d.questionModel.Question, 0, ((QustionsPage.Dimensions.Height - 100) / 5 * i), QustionsPage.Dimensions.Width - 100, questionHeight, Font.Helvetica, 14, TextAlign.Left));
                     lineText += "Answer 1: " + d.questionModel.Answer1 + "\n";
                     lineText += "Answer 2: " + d.questionModel.Answer2 + "\n";
                     lineText += "Answer 3: " + d.questionModel.Answer3 + "\n";
@@ -109,24 +118,24 @@ namespace fbla.Models
                     lineText += "Answer Selected: " + d.questionModel.answerSelected + "\n";
                     lineText += "Point(s): " + d.score + "/1" + "\n";
                     lineText += "\n";
-                    page.Elements.Add(new Label(lineText, 0, ((page.Dimensions.Height - 100) / 5 * i) + (page.Dimensions.Height / 25), page.Dimensions.Width - 100, answerHeight, Font.Helvetica, 12, TextAlign.Left));
+                    QustionsPage.Elements.Add(new Label(lineText, 0, ((QustionsPage.Dimensions.Height - 100) / 5 * i) + (QustionsPage.Dimensions.Height / 25), QustionsPage.Dimensions.Width - 100, answerHeight, Font.Helvetica, 12, TextAlign.Left));
                 }
                 else if (d.GetType() == (new TrueFalseQuestionViewModel()).GetType())
                 {
                     string lineText = "";
-                    page.Elements.Add(new Label("Question: " + d.questionModel.Question, 0, ((page.Dimensions.Height - 100) / 5 * i), page.Dimensions.Width - 100, questionHeight, Font.Helvetica, 14, TextAlign.Left));
+                    QustionsPage.Elements.Add(new Label("Question: " + d.questionModel.Question, 0, ((QustionsPage.Dimensions.Height - 100) / 5 * i), QustionsPage.Dimensions.Width - 100, questionHeight, Font.Helvetica, 14, TextAlign.Left));
                     lineText += "Answer 1: " + d.questionModel.Answer1 + "\n";
                     lineText += "Answer 2: " + d.questionModel.Answer2 + "\n";
                     lineText += "Correct Answer: " + d.questionModel.correctAnswer + "\n";
                     lineText += "Answer Selected: " + d.questionModel.answerSelected + "\n";
                     lineText += "Point(s): " + d.score + "/1" + "\n";
                     lineText += "\n";
-                    page.Elements.Add(new Label(lineText, 0, ((page.Dimensions.Height - 100) / 5 * i) + (page.Dimensions.Height / 25), page.Dimensions.Width - 100, answerHeight, Font.Helvetica, 12, TextAlign.Left));
+                    QustionsPage.Elements.Add(new Label(lineText, 0, ((QustionsPage.Dimensions.Height - 100) / 5 * i) + (QustionsPage.Dimensions.Height / 25), QustionsPage.Dimensions.Width - 100, answerHeight, Font.Helvetica, 12, TextAlign.Left));
                 }
                 else if (d.GetType() == (new FillBlankQuestionViewModel()).GetType())
                 {
                     string lineText = "";
-                    page.Elements.Add(new Label("Question: " + d.questionModel.Question, 0, ((page.Dimensions.Height - 100) / 5 * i), page.Dimensions.Width - 100, questionHeight, Font.Helvetica, 14, TextAlign.Left));
+                    QustionsPage.Elements.Add(new Label("Question: " + d.questionModel.Question, 0, ((QustionsPage.Dimensions.Height - 100) / 5 * i), QustionsPage.Dimensions.Width - 100, questionHeight, Font.Helvetica, 14, TextAlign.Left));
                     lineText += "Answer 1: " + d.questionModel.Answer1 + "\n";
                     lineText += "Answer 2: " + d.questionModel.Answer2 + "\n";
                     lineText += "Answer 3: " + d.questionModel.Answer3 + "\n";
@@ -135,12 +144,12 @@ namespace fbla.Models
                     lineText += "Answer Selected: " + d.questionModel.answerSelected + "\n";
                     lineText += "Point(s): " + d.score + "/1" + "\n";
                     lineText += "\n";
-                    page.Elements.Add(new Label(lineText, 0, ((page.Dimensions.Height - 100) / 5 * i) + (page.Dimensions.Height / 25), page.Dimensions.Width - 100, answerHeight, Font.Helvetica, 12, TextAlign.Left));
+                    QustionsPage.Elements.Add(new Label(lineText, 0, ((QustionsPage.Dimensions.Height - 100) / 5 * i) + (QustionsPage.Dimensions.Height / 25), QustionsPage.Dimensions.Width - 100, answerHeight, Font.Helvetica, 12, TextAlign.Left));
                 }
                 else if (d.GetType() == (new CheckboxQuestionViewModel()).GetType())
                 {
                     string lineText = "";
-                    page.Elements.Add(new Label("Question: " + d.questionModel.Question, 0, ((page.Dimensions.Height - 100) / 5 * i), page.Dimensions.Width - 100, questionHeight, Font.Helvetica, 14, TextAlign.Left));
+                    QustionsPage.Elements.Add(new Label("Question: " + d.questionModel.Question, 0, ((QustionsPage.Dimensions.Height - 100) / 5 * i), QustionsPage.Dimensions.Width - 100, questionHeight, Font.Helvetica, 14, TextAlign.Left));
                     lineText += "Answer 1: " + d.questionModel.Answer1 + "\n";
                     lineText += "Answer 2: " + d.questionModel.Answer2 + "\n";
                     lineText += "Answer 3: " + d.questionModel.Answer3 + "\n";
@@ -159,216 +168,14 @@ namespace fbla.Models
                     }
                     lineText += "Point(s): " + d.score + "/1" + "\n";
                     lineText += "\n";
-                    page.Elements.Add(new Label(lineText, 0, ((page.Dimensions.Height - 100) / 5 * i) + (page.Dimensions.Height / 25), page.Dimensions.Width - 100, answerHeight, Font.Helvetica, 12, TextAlign.Left));
+                    QustionsPage.Elements.Add(new Label(lineText, 0, ((QustionsPage.Dimensions.Height - 100) / 5 * i) + (QustionsPage.Dimensions.Height / 25), QustionsPage.Dimensions.Width - 100, answerHeight, Font.Helvetica, 12, TextAlign.Left));
                 }
                 i++;
             }
             document.Draw(path + pathPrefixString + ".pdf");
         }
 
-        //puts results data into text files in the documents folder, no longer used
-        /*
-        public void printFormatter(List<dynamic> questionsList)
-        {
-            DateTime currentTime = DateTime.Now;
-            char[] pathPrefix = currentTime.ToString().ToCharArray();
-            for (int i = 0; i <pathPrefix.Length; i++)
-            {
-                if (pathPrefix[i] == '/' || pathPrefix[i] == '\\' || pathPrefix[i] == ':' ) {
-                    pathPrefix[i] = ' ';
-                }
-            }
-            var documentsPath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            Directory.CreateDirectory(documentsPath + "\\fblaResults\\");
-            String pathPrefixString = String.Concat(pathPrefix.Where(c => !Char.IsWhiteSpace(c)));
-            pathPrefixString = pathPrefixString.Substring(0, pathPrefixString.Length - 2);
-            String path = documentsPath + "\\fblaResults\\";
-            using (StreamWriter sr = File.CreateText(path + pathPrefixString + @".txt"))
-            {
-                foreach (dynamic d in questionsList) {
-                    if (d.GetType() == (new MultipleChoiceQuestionViewModel()).GetType())
-                    {
-
-                        sr.WriteLine("Question: " + d.questionModel.Question);
-                        sr.WriteLine("Answer 1: " + d.questionModel.Answer1);
-                        sr.WriteLine("Answer 2: " + d.questionModel.Answer2);
-                        sr.WriteLine("Answer 3: " + d.questionModel.Answer3);
-                        sr.WriteLine("Answer 4: " + d.questionModel.Answer4);
-                        switch (d.questionModel.correctAnswer)
-                        {
-                            case 1:
-                                sr.WriteLine("Correct Answer: " + d.questionModel.Answer1);
-                                break;
-                            case 2:
-                                sr.WriteLine("Correct Answer: " + d.questionModel.Answer2);
-                                break;
-                            case 3:
-                                sr.WriteLine("Correct Answer: " + d.questionModel.Answer3);
-                                break;
-                            case 4:
-                                sr.WriteLine("Correct Answer: " + d.questionModel.Answer4);
-                                break;
-                        }
-                        switch (d.questionModel.answerSelected)
-                        {
-                            case 1:
-                                sr.WriteLine("Answer selected: " + d.questionModel.Answer1);
-                                break;
-                            case 2:
-                                sr.WriteLine("Answer selected: " + d.questionModel.Answer2);
-                                break;
-                            case 3:
-                                sr.WriteLine("Answer selected: " + d.questionModel.Answer3);
-                                break;
-                            case 4:
-                                sr.WriteLine("Answer selected: " + d.questionModel.Answer4);
-                                break;
-                        }
-                        sr.WriteLine("Point(s): " + d.score + "/1");
-                        sr.WriteLine();
-                    }
-                    else if (d.GetType() == (new TrueFalseQuestionViewModel()).GetType())
-                    {
-                        sr.WriteLine("Question: " + d.questionModel.Question);
-                        sr.WriteLine("Answer 1: " + d.questionModel.Answer1);
-                        sr.WriteLine("Answer 2: " + d.questionModel.Answer2);
-                        switch (d.questionModel.correctAnswer)
-                        {
-                            case 1:
-                                sr.WriteLine("Correct Answer: " + d.questionModel.Answer1);
-                                break;
-                            case 2:
-                                sr.WriteLine("Correct Answer: " + d.questionModel.Answer2);
-                                break;
-                        }
-                        switch (d.questionModel.answerSelected)
-                        {
-                            case 1:
-                                sr.WriteLine("Answer selected: " + d.questionModel.Answer1);
-                                break;
-                            case 2:
-                                sr.WriteLine("Answer selected: " + d.questionModel.Answer2);
-                                break;
-                        }
-                        sr.WriteLine("Point(s): " + d.score + "/1");
-                        sr.WriteLine();
-                    }
-                    else if (d.GetType() == (new FillBlankQuestionViewModel()).GetType())
-                    {
-
-
-                        sr.WriteLine("Question: " + d.questionModel.Question);
-                        sr.WriteLine("Answer 1: " + d.questionModel.Answer1);
-                        sr.WriteLine("Answer 2: " + d.questionModel.Answer2);
-                        sr.WriteLine("Answer 3: " + d.questionModel.Answer3);
-                        sr.WriteLine("Answer 4: " + d.questionModel.Answer4);
-                        switch (d.questionModel.correctAnswer)
-                                {
-                                    case 1:
-                                        sr.WriteLine("Correct Answer: " + d.questionModel.Answer1);
-                                        break;
-                                    case 2:
-                                        sr.WriteLine("Correct Answer: " + d.questionModel.Answer2);
-                                        break;
-                                    case 3:
-                                        sr.WriteLine("Correct Answer: " + d.questionModel.Answer3);
-                                        break;
-                                    case 4:
-                                        sr.WriteLine("Correct Answer: " + d.questionModel.Answer4);
-                                        break;
-                                }
-                                switch (d.questionModel.answerSelected)
-                                {
-                                    case 1:
-                                        sr.WriteLine("Answer selected: " + d.questionModel.Answer1);
-                                        break;
-                                    case 2:
-                                        sr.WriteLine("Answer selected: " + d.questionModel.Answer2);
-                                        break;
-                                    case 3:
-                                        sr.WriteLine("Answer selected: " + d.questionModel.Answer3);
-                                        break;
-                                    case 4:
-                                        sr.WriteLine("Answer selected: " + d.questionModel.Answer4);
-                                        break;
-                                }
-                                sr.WriteLine("Point(s): " + d.score + "/1");
-                                sr.WriteLine();
-                     }
-                    else if (d.GetType() == (new CheckboxQuestionViewModel()).GetType())
-                    {
-                        sr.WriteLine("Question: " + d.questionModel.Question);
-                        sr.WriteLine("Answer 1: " + d.questionModel.Answer1);
-                        sr.WriteLine("Answer 2: " + d.questionModel.Answer2);
-                        sr.WriteLine("Answer 3: " + d.questionModel.Answer3);
-                        sr.WriteLine("Answer 4: " + d.questionModel.Answer4);
-                        switch (d.questionModel.correctAnswer1)
-                                {
-                                    case 1:
-                                        sr.WriteLine("Correct Answer 1: " + d.questionModel.Answer1);
-                                        break;
-                                    case 2:
-                                        sr.WriteLine("Correct Answer 1: " + d.questionModel.Answer2);
-                                        break;
-                                    case 3:
-                                        sr.WriteLine("Correct Answer 1: " + d.questionModel.Answer3);
-                                        break;
-                                    case 4:
-                                        sr.WriteLine("Correct Answer 1: " + d.questionModel.Answer4);
-                                        break;
-                                }
-                                switch (d.questionModel.correctAnswer2)
-                                {
-                                    case 1:
-                                        sr.WriteLine("Correct Answer 2: " + d.questionModel.Answer1);
-                                        break;
-                                    case 2:
-                                        sr.WriteLine("Correct Answer 2: " + d.questionModel.Answer2);
-                                        break;
-                                    case 3:
-                                        sr.WriteLine("Correct Answer 2: " + d.questionModel.Answer3);
-                                        break;
-                                    case 4:
-                                        sr.WriteLine("Correct Answer 2: " + d.questionModel.Answer4);
-                                        break;
-                                }
-                                switch (d.choicesSelected[0])
-                                {
-                                    case 1:
-                                        sr.WriteLine("Answer selected 1: " + d.questionModel.Answer1);
-                                        break;
-                                    case 2:
-                                        sr.WriteLine("Answer selected 1: " + d.questionModel.Answer2);
-                                        break;
-                                    case 3:
-                                        sr.WriteLine("Answer selected 1: " + d.questionModel.Answer3);
-                                        break;
-                                    case 4:
-                                        sr.WriteLine("Answer selected 1: " + d.questionModel.Answer4);
-                                        break;
-                                }
-                                switch (d.choicesSelected[1])
-                                {
-                                    case 1:
-                                        sr.WriteLine("Answer selected 2: " + d.questionModel.Answer1);
-                                        break;
-                                    case 2:
-                                        sr.WriteLine("Answer selected 2: " + d.questionModel.Answer2);
-                                        break;
-                                    case 3:
-                                        sr.WriteLine("Answer selected 2: " + d.questionModel.Answer3);
-                                        break;
-                                    case 4:
-                                        sr.WriteLine("Answer selected 2: " + d.questionModel.Answer4);
-                                        break;
-                                }
-                                sr.WriteLine("Point(s): " + d.score + "/1");
-                                sr.WriteLine();
-                            }
-                }
-            }
-        }
-        */
+        
         
     }
 }
